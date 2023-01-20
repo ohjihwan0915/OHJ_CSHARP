c044724f7ac8b7aa1dbf39a623420de69c25c0fd
ohjihwan <ohjihwan915@gmail.com>
2023-01-20 19:44:19 +0900

    임시테스트

--- 20230120/Program.cs
+++ 20230120/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+#nullable disable
+/*Console.Write(":");
+int a = int.Parse(Console.ReadLine());
+Console.Write(":");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine($"{a} + {b} = {a+b}");
+*/
+
+string name = Console.ReadLine();
+int age = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{name}은 {2023 + 100 - age}에 백 살이 됩니다");
