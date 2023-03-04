3e1f0d12ebdd2ba1cb83abadf713cf293a28493d
ohjihwan0915 <77764254+ohjihwan0915@users.noreply.github.com>
2023-03-04 14:24:24 +0900

    .
    a

---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿//2차원배열 1-1
+/*
+int n = 4;
+
+for(int i = 0; i < n; i++) {
+    for(int j = 1; j <= n; j++) {
+        Console.Write(j + i*n);
+    }Console.WriteLine();
+}
+*/
+
+//2차원 배열 1-2
+/*
+int n = 5;
+
+for(int i = 0; i < n; i++) {
+    for(int j = n; j > 0; j--) {
+        Console.Write(j + i*n);
+    }Console.WriteLine();
+}
+*/
+//2차원 배열 1-3
+/*
+int n = 5;
+for(int i = 1; i <= n; i++) {
+    for(int j = 0; j < n; j++) {
+        Console.Write(i + n*j);
+    }Console.WriteLine();
+}
+*/
+
+//2차원 배열 1-4
+
+/*
+int n = 5;
+
+for(int i = n; i > 0; i--) {
+    for(int j = 0; j < n; j++) {
+        Console.Write(i + j*n);
+    }Console.WriteLine();
+}
+*/
+
+//2차월 배열 1-5
+/*
+
+int n = 4;
+int m = 5;
+
+for(int i = n; i > 0; i--) {
+    for(int j = 0; j < m; j++) {
+        Console.Write(i * m - j);
+    }Console.WriteLine();
+}
+*/
+//2차원 배열 1-6
+
+/*
+int n = 4;
+int m = 5;
+
+for(int i = n; i > 0; i--) {
+    for(int j = n; j >= 0; j--) {
+        Console.Write(i*m - j);
+    }Console.WriteLine();
+}
+*/
+
+//2차원 배열 1-7
+
+/*
+int n = 4;
+int m = 5;
+
+for(int i = 0; i < n; i++) {
+    for(int j = 0; j <= n; j++) {
+        Console.Write(n*m-i-j*n);
+    }Console.WriteLine();
+}
+*/
+
+//2차원 배열 1-8
+/*
+int n = 4;
+int m = 5;
+
+for(int i = n-1; i >= 0; i--) {
+    for(int j = 0; j <= n; j++) {
+        Console.Write(n*m-i-j*n);
+    }Console.WriteLine();
+}
+*/
